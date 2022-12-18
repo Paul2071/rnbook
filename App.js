5f2d83141dd6b98522b71684ac2528cd2db731a0
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2022-12-18 14:21:20 +0000

    first play with making a todo app. Added branches to keep it all separate.

--- App.js
+++ App.js
@@ -1,150 +1,39 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { 
-  Button, 
-  StyleSheet, 
-  Text, 
-  View, 
-  TextInput, 
-  ScrollView,
-  FlatList,
-  TouchableOpacity  } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function App() {
-  const [name, setName] = useState();
-  const [age, setAge] = useState();
-  const [ people, setPeople ] = useState([
-    {name: "Fjor", id: 1},
-    {name: "Magne", id: 2 },
-    {name: "Laurits", id: 3 },
-    {name: "Saxa", id: 4},
-    {name: "Isolde", id: 5},
-    {name: "Edda", id: 6},
-    {name: "Iman", id: 7},
-    {name: "Odin", id: 8},
-  ])
-
-const pressHandler = (id) => {
-  console.log(id);
-  setPeople( (previousPeople) => {
-    return previousPeople.filter( ( person ) => person.id != id );
-  }
-    )
-} 
-
-const resetState = () => {
-  setPeople([
-    {name: "Fjor", id: 1},
-    {name: "Magne", id: 2 },
-    {name: "Laurits", id: 3 },
-    {name: "Saxa", id: 4},
-    {name: "Isolde", id: 5},
-    {name: "Edda", id: 6},
-    {name: "Iman", id: 7},
-    {name: "Odin", id: 8},
-  ])
-}
-
+  const [todos, setTodos] = useState([
+    { text: "Listen to music", key: "1" },
+    { text: "Practice coding", key: "2" },
+    { text: "Go to the gym", key: "3" },
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text>What is your name?</Text>
-
-      <TextInput
-        style={styles.input}
-        placeholder="Enter name here..."
-        onChangeText={(value) => setName(value)}
-      />
-      <Text>What is your age?</Text>
-
-      <TextInput
-        keyboardType="numeric"
-        style={styles.age}
-        placeholder="Enter age here..."
-        onChangeText={(value) => setAge(value)}
-      />
-
-      <View style={styles.header}>
-        <Text style={styles.boldText}>My name is {name} and I am {age} years old</Text>
+      {/* header */}
+      <View style={styles.content}>
+        {/* to form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ ele }) => (<Text>{ele.text}</Text>)}
+          />
+        </View>
       </View>
-
-        
-      <Button 
-      title={"reset"} 
-      onPress={resetState}
-      />
-
-
-
-      <FlatList 
-        numColumns={4}        
-        keyExtractor={(item) => item.id}
-        data={people}
-        renderItem={( { item } ) => (
-        <TouchableOpacity onPress={ () => pressHandler(item.id)} >    
-          <Text style={styles.element}> {item.name}</Text>
-         </TouchableOpacity>  
-
-        )}
-      />
-      
-
-      {/* <ScrollView>
-       {people.map((element) => {
-        return (
-          <View key={element.key}>
-            <Text style={styles.element}>{element.name}</Text>
-          </View>
-        )
-        })}
-      </ScrollView>  */}
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  element: {
-    marginTop: 10,
-    marginLeft: 10,
-    padding: 15,
-    backgroundColor: "teal",
-    fontSize: 15
-  },
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
-  header: {
-    backgroundColor: "pink",
-    padding: 20,
+  content: {
+    padding: 400,
   },
-  boldText: {
-    fontWeight: "bold",
-    color: "red",
-  },
-  body: {
-    backgroundColor: "purple",
-    padding: 20,
-    fontWeight: "bold",
-  },
-  buttonContainer: {
+  list: {
     marginTop: 20,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#777",
-    padding: 8,
-    margin: 10,
-    width: 150,
-  },
-  age: {
-    borderWidth: 1,
-    borderColor: "#777",
-    padding: 8,
-    margin: 10,
-    width: 150,
   },
 });
