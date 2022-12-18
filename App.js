bf9e923ee4027fc500001347af346ba6b0e5be60
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2022-12-18 14:41:53 +0000

    fixed issue with list no rendering - the rederitem function was missing brackets, fixed that, then the styling i had added to try and make anything show was messing up where it rendered

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   const [todos, setTodos] = useState([
-    { text: "Listen to music", key: "1" },
-    { text: "Practice coding", key: "2" },
-    { text: "Go to the gym", key: "3" },
+    { task: "Listen to music", key: "1" },
+    { task: "Practice coding", key: "2" },
+    { task: "Go to the gym", key: "3" },
   ]);
 
   return (
     <View style={styles.container}>
-      {/* header */}
       <View style={styles.content}>
-        {/* to form */}
         <View style={styles.list}>
           <FlatList
+            keyExtractor={(item) => item.key}
             data={todos}
-            renderItem={({ ele }) => (<Text>{ele.text}</Text>)}
+            renderItem={({ item }) => <Text>{item.task}</Text>}
           />
         </View>
       </View>
@@ -31,9 +36,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   content: {
-    padding: 400,
+    padding: 40,
   },
   list: {
     marginTop: 20,
+    padding: 40,
+   
   },
 });
